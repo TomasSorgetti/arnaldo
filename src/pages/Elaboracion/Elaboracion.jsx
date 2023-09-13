35f7273866022c717c0b71cedd8edcd94ee2a75a
TomasSorgetti <97346262+TomasSorgetti@users.noreply.github.com>
2023-09-13 13:35:48 -0300

    historia y elaboracion

--- src/pages/Elaboracion/Elaboracion.jsx
+++ src/pages/Elaboracion/Elaboracion.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import procesos from "../../assets/images/imagenes_elaboracion/procesos.png";
+import controles from "../../assets/images/imagenes_elaboracion/controles.png";
+import elaboracion from "../../assets/images/imagenes_elaboracion/elaboracion.png";
 
 const Elaboracion = () => {
   return (
-    <div>Elaboracion</div>
-  )
-}
+    <section className="font-heebo">
+      <article className="relative h-[132px] overflow-hidden">
+        <img
+          className="object-cover w-full"
+          src={elaboracion}
+          alt="historia header"
+        />
+        <h3 className="absolute uppercase left-4 bottom-0 text-[2.5rem] font-black">
+          elaboración
+        </h3>
+      </article>
+      <section className="flex flex-col gap-10 py-5">
+        <article className="w-11/12 m-auto">
+          <h3 className="text-[1.5rem] font-extrabold uppercase">procesos</h3>
+          <div className="flex flex-col gap-4 text-[1rem] font-normal">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet
+              facilisis magna etiam tempor orci eu lobortis elementum. Vitae
+              tortor condimentum lacinia quis. Arcu risus quis varius quam
+              quisque id diam. Interdum consectetur libero id faucibus.
+            </p>
+            <p>
+              Pellentesque habitant morbi tristique senectus et netus et. Sem
+              viverra aliquet eget sit amet tellus cras. Elementum nibh tellus
+              molestie nunc non blandit. Parturient montes nascetur ridiculus
+              mus mauris vitae ultricies leo integer. Eget lorem dolor sed
+              viverra ipsum. Arcu dictum varius duis at consectetur lorem donec
+              massa sapien. At urna condimentum mattis pellentesque id. Massa
+              massa ultricies mi quis hendrerit dolor. Commodo odio aenean sed
+              adipiscing diam.
+            </p>
+          </div>
+          <img className="w-full mt-3" src={procesos} alt="procesos del helado" />
+        </article>
+        <article className="w-11/12 m-auto">
+          <h3 className="text-[1.5rem] font-extrabold uppercase">controles</h3>
+          <div className="flex flex-col gap-4 text-[1rem] font-normal">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet
+              facilisis magna etiam tempor orci eu lobortis elementum. Vitae
+              tortor condimentum lacinia quis. Arcu risus quis varius quam
+              quisque id diam. Interdum consectetur libero id faucibus.
+            </p>
+            <p>
+              Pellentesque habitant morbi tristique senectus et netus et. Sem
+              viverra aliquet eget sit amet tellus cras. Elementum nibh tellus
+              molestie nunc non blandit. Parturient montes nascetur ridiculus
+              mus mauris vitae ultricies leo integer. Eget lorem dolor sed
+              viverra ipsum. Arcu dictum varius duis at consectetur lorem donec
+              massa sapien. At urna condimentum mattis pellentesque id. Massa
+              massa ultricies mi quis hendrerit dolor. Commodo odio aenean sed
+              adipiscing diam.
+            </p>
+          </div>
+          <img className="w-full mt-3" src={controles} alt="controles del helado" />
+        </article>
+      </section>
+    </section>
+  );
+};
 
-export default Elaboracion
+export default Elaboracion;
--- src/pages/Historia/Historia.jsx
+++ src/pages/Historia/Historia.jsx
@@ -1,9 +1,96 @@
-import React from 'react'
+import historia from "../../assets/images/imagenes_home/cards_home/card_home_historia.webp";
+import empresa from "../../assets/images/imagenes_historia/empresa.png";
+import marca from "../../assets/images/imagenes_historia/marca.png";
+import arnaldo from "../../assets/images/imagenes_historia/arnaldo.png";
 
 const Historia = () => {
   return (
-    <div>Historia</div>
-  )
-}
+    <section className="font-heebo  border-b-2 border-white">
+      <article className="relative h-[132px] overflow-hidden">
+        <img className="object-cover" src={historia} alt="historia header" />
+        <h3 className="absolute uppercase left-4 bottom-0 text-[2.5rem] font-black">
+          historia
+        </h3>
+      </article>
+      <section className="flex flex-col gap-2 py-5">
+        <article className="w-11/12 m-auto">
+          <h3 className="text-[1.5rem] font-extrabold uppercase">
+            sobre la empresa
+          </h3>
+          <div className="flex flex-col gap-4 text-[1rem] font-normal">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet
+              facilisis magna etiam tempor orci eu lobortis elementum. Vitae
+              tortor condimentum lacinia quis. Arcu risus quis varius quam
+              quisque id diam. Interdum consectetur libero id faucibus.
+            </p>
+            <p>
+              Pellentesque habitant morbi tristique senectus et netus et. Sem
+              viverra aliquet eget sit amet tellus cras. Elementum nibh tellus
+              molestie nunc non blandit. Parturient montes nascetur ridiculus
+              mus mauris vitae ultricies leo integer. Eget lorem dolor sed
+              viverra ipsum. Arcu dictum varius duis at consectetur lorem donec
+              massa sapien. At urna condimentum mattis pellentesque id. Massa
+              massa ultricies mi quis hendrerit dolor. Commodo odio aenean sed
+              adipiscing diam.
+            </p>
+          </div>
+          <img className="w-full mt-3" src={empresa} alt="" />
+        </article>
+        <article className="w-11/12 m-auto">
+          <h3 className="text-[1.5rem] font-extrabold uppercase">
+            NUESTRA MARCA
+          </h3>
+          <div className="flex flex-col gap-4 text-[1rem] font-normal">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet
+              facilisis magna etiam tempor orci eu lobortis elementum. Vitae
+              tortor condimentum lacinia quis. Arcu risus quis varius quam
+              quisque id diam. Interdum consectetur libero id faucibus.
+            </p>
+            <p>
+              Pellentesque habitant morbi tristique senectus et netus et. Sem
+              viverra aliquet eget sit amet tellus cras. Elementum nibh tellus
+              molestie nunc non blandit. Parturient montes nascetur ridiculus
+              mus mauris vitae ultricies leo integer. Eget lorem dolor sed
+              viverra ipsum. Arcu dictum varius duis at consectetur lorem donec
+              massa sapien. At urna condimentum mattis pellentesque id. Massa
+              massa ultricies mi quis hendrerit dolor. Commodo odio aenean sed
+              adipiscing diam.
+            </p>
+          </div>
+          <img className="w-full mt-3" src={marca} alt="historia de la marca" />
+        </article>
+        <article className="w-11/12 m-auto">
+          <h3 className="text-[1.5rem] font-extrabold uppercase">
+            AGUSTÍN ARNALDO
+          </h3>
+          <div className="flex flex-col gap-4 text-[1rem] font-normal">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet
+              facilisis magna etiam tempor orci eu lobortis elementum. Vitae
+              tortor condimentum lacinia quis. Arcu risus quis varius quam
+              quisque id diam. Interdum consectetur libero id faucibus.
+            </p>
+            <p>
+              Pellentesque habitant morbi tristique senectus et netus et. Sem
+              viverra aliquet eget sit amet tellus cras. Elementum nibh tellus
+              molestie nunc non blandit. Parturient montes nascetur ridiculus
+              mus mauris vitae ultricies leo integer. Eget lorem dolor sed
+              viverra ipsum. Arcu dictum varius duis at consectetur lorem donec
+              massa sapien. At urna condimentum mattis pellentesque id. Massa
+              massa ultricies mi quis hendrerit dolor. Commodo odio aenean sed
+              adipiscing diam.
+            </p>
+          </div>
+          <img className="w-full mt-3" src={arnaldo} alt="arnaldo" />
+        </article>
+      </section>
+    </section>
+  );
+};
 
-export default Historia
+export default Historia;
