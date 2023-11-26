6a342518d0711378406feec8b96e9d9b9b330793
TomasSorgetti <97346262+TomasSorgetti@users.noreply.github.com>
2023-11-26 15:07:17 -0300

    reseñas carousel

---
+++ src/components/Reseñas/Carousel.jsx
@@ -0,0 +1,70 @@
+import AliceCarousel from "react-alice-carousel";
+import cards from "./cards";
+import { useState, useEffect } from "react";
+import greenBalls from "../../assets/images/iconos/icono_opinion_tripadvisor/icono_opinion_tripadvisor.png";
+
+const Carousel = () => {
+    const [items, setItems] = useState([]);
+    useEffect(() => {
+      const arr = [];
+      cards?.map((card,index) => {
+        arr.push(
+            <div
+              key={index}
+              onDragStart={handleDragStart}
+              role="presentation"
+              className="mb-5 w-[330px] h-[260px] border-[1px] shadow-xl rounded-[25px] 2xl:w-[419px] 2xl:h-[334px]"
+            >
+              <div className="flex items-center gap-2 p-6 ">
+                <img
+                  className="w-auto h-4"
+                  src={greenBalls}
+                  alt="icon tripadvisor"
+                />
+                <span className="text-xs">{card.date}</span>
+              </div>
+              <div className="px-6 h-[140px] flex flex-col gap-2 2xl:gap-6">
+                <h5 className="text-xl font-normal lg:text-[1.2rem] 2xl:text-[1.5rem]">
+                  {card.name}
+                </h5>
+                <p className="text-base font-normal lg:text-[1.1rem] 2xl:text-[1.3rem]">
+                  {card.text}
+                </p>
+              </div>
+            </div>
+        );
+      });
+      setItems(arr);
+    }, []);
+    const handleDragStart = (e) => e.preventDefault();
+
+    const responsive = {
+      0: {
+        items: 1,
+      },
+      640: {
+        items: 1.7,
+      },
+      768: {
+        items: 2,
+      },
+      1024: {
+        items: 2.8,
+      },
+      1280: {
+        items: 3.5,
+      },
+      1440: {
+        items: 3.1,
+      },
+      1680: {
+        items: 3.6,
+      },
+      1920: {
+        items: 4.2,
+      },
+    };
+  return <AliceCarousel disableDotsControls items={items} responsive={responsive} />;
+}
+
+export default Carousel
--- src/components/Reseñas/Reseñas.jsx
+++ src/components/Reseñas/Reseñas.jsx
@@ -1,17 +1,10 @@
 import pngwing from "../../assets/images/imagenes_home/opiniones_tripadvisor_home/pngwing.com.png";
-import greenBalls from "../../assets/images/iconos/icono_opinion_tripadvisor/icono_opinion_tripadvisor.png";
 import { useState, useEffect, useRef } from "react";
 import { motion } from "framer-motion";
-import cards from "./cards";
+import Carousel from "./Carousel";
 
 
 const Reseñas = () => {
-  const carousel = useRef();
-  const [width, setWidth] = useState(0);
-  useEffect(() => {
-    setWidth(carousel.current?.scrollWidth - carousel.current?.offsetWidth);
-  }, []);
-
   return (
     <div className="bg-white text-black pb-4 pr-0 relative font-heebo lg:pl-8 2xl:pl-16">
       <motion.div
@@ -24,7 +17,7 @@ const Reseñas = () => {
         transition={{ duration: 0.6 }}
         className="font-black flex justify-between w-full items-center"
       >
-        <div>
+        <div className="pl-4">
           <h3 className="text-[2rem] font-heebo lg:text-[3rem] 2xl:text-[4.8rem]">
             Nuestros clientes
           </h3>
@@ -38,42 +31,10 @@ const Reseñas = () => {
           alt="tripadvisor logo"
         />
       </motion.div>
-      <motion.div
-        ref={carousel}
-        className="w-full overflow-hidden"
-        whileTap={{ cursor: "grabbing" }}
-      >
         <motion.div
-          className="w-full h-[300px] flex gap-2 relative lg:mb-5 2xl:h-[400px] 2xl:gap-4"
-          drag="x"
-          dragConstraints={{ right: 0, left: -width }}
+          className="reseñas_carousel w-full my-5 pl-4 sm:pl-7 lg:pl-4"
         >
-          {cards.map((card, index) => (
-            <div
-              key={index}
-              className="w-screen flex justify-center items-center"
-            >
-              <div className="w-[330px] h-[260px] border-[1px] shadow-xl rounded-[25px] 2xl:w-[419px] 2xl:h-[334px]">
-                <div className="flex items-center gap-2 p-6 ">
-                  <img
-                    className="w-auto h-4"
-                    src={greenBalls}
-                    alt="icon tripadvisor"
-                  />
-                  <span className="text-xs">{card.date}</span>
-                </div>
-                <div className="px-6 h-[140px] flex flex-col gap-2 2xl:gap-6">
-                  <h5 className="text-xl font-normal lg:text-[1.2rem] 2xl:text-[1.5rem]">
-                    {card.name}
-                  </h5>
-                  <p className="text-base font-normal lg:text-[1.1rem] 2xl:text-[1.3rem]">
-                    {card.text}
-                  </p>
-                </div>
-              </div>
-            </div>
-          ))}
-        </motion.div>
+          <Carousel/>
       </motion.div>
       <div className="w-full pb-6 bg-white flex justify-center lg:pb-8 2xl:pb-10 2xl:pr-16">
         <button className="uppercase w-11/12 rounded bg-black text-white text-center py-2 text-[0.9rem] font-bold lg:w-[400px] 2xl:py-3 hover:bg-hoverButton">
