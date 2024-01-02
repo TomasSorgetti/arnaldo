dbe13652a3a3669a8d9c04e4d2891aab8233b8c3
TomasSorgetti <97346262+TomasSorgetti@users.noreply.github.com>
2024-01-02 18:24:42 -0300

    error page button style

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -9,7 +9,7 @@ const Error = () => {
   return (
     <div className="font-heebo h-[100svh] flex flex-col justify-between">
       <Navigation activeNav={activeNav} setActiveNav={setActiveNav} />
-      <div className="flex flex-col gap-8 items-center text-center">
+      <div className="flex flex-col gap-8 items-center text-center lg:pb-6">
         <img
           className="mt-24 mb-10 w-10/12 sm:w-9/12 lg:w-5/12 lg:mt-32 xl:w-4/12"
           src={errorImage}
@@ -19,7 +19,7 @@ const Error = () => {
           Parece que la página que estás buscando no se encuentra disponible
         </p>
         <Link
-          className="text-[1rem] md:text-[1.2rem] uppercase py-2 px-8 border-2 border-white rounded-md font-bold"
+          className="text-[1rem] md:text-[1.2rem] uppercase pt-2 pb-1 px-8 border-2 border-white rounded-md font-bold hover:bg-[#252424] active:bg-[#494646] focus:bg-[#676565]"
           to="/"
         >
           Volver al inicio
